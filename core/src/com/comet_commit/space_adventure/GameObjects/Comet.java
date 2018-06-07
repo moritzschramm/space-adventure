624d752b6f293345999ff0b0389ea18906ea98a5
Moritz Schramm <moritz.schramm1@gmail.com>
2018-06-07 14:37:58 +0200

    add getTexture() and add parameter internalPath to constructor of GameObject

--- core/src/com/comet_commit/space_adventure/GameObjects/Comet.java
+++ core/src/com/comet_commit/space_adventure/GameObjects/Comet.java
@@ -1,14 +1,8 @@
 package com.comet_commit.space_adventure.GameObjects;
 
-import com.badlogic.gdx.graphics.Texture;
-
 public class Comet extends GameObject {
 
-
     public Comet(int x, int y) {
-        super(x, y);
-        super.setTexture(new Texture("comet.png"));
+        super(x, y, "comet.png");
     }
-
-
 }
