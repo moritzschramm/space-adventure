9a42ee2b280fe51dce82c345150b05a82f5e3ed6
Moritz Schramm <moritz.schramm1@gmail.com>
2018-06-07 11:27:08 +0200

    comet commit

---
+++ android/src/com/comet_commit/space_adventure/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.comet_commit.space_adventure;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.comet_commit.space_adventure.SpaceAdventure;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new SpaceAdventure(), config);
+	}
+}
---
+++ desktop/src/com/comet_commit/space_adventure/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.comet_commit.space_adventure.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.comet_commit.space_adventure.SpaceAdventure;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SpaceAdventure(), config);
+	}
+}
