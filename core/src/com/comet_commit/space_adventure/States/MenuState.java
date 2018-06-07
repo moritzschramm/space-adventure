58249ce898b04faa5da611938632e449d0cd3367
Moritz Andres <moritz@moritzandres.net>
2018-06-07 15:15:03 +0200

    implement MenuState

--- core/src/com/comet_commit/space_adventure/States/MenuState.java
+++ core/src/com/comet_commit/space_adventure/States/MenuState.java
@@ -1,12 +1,17 @@
 package com.comet_commit.space_adventure.States;
 
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class MenuState extends State {
 
+    Texture background;
+    Texture playBtn;
+
     public MenuState(GameStateManager gsm) {
         super(gsm);
-
+        background = new Texture("background.png");
+        playBtn = new Texture("playbtn.png");
     }
 
     @Override
