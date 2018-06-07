9c8d4144dcc1ae30fee79d99484502a5d2dd58ea
Moritz Andres <moritz@moritzandres.net>
2018-06-07 15:18:33 +0200

    implement MenuState

--- core/src/com/comet_commit/space_adventure/States/MenuState.java
+++ core/src/com/comet_commit/space_adventure/States/MenuState.java
@@ -1,5 +1,6 @@
 package com.comet_commit.space_adventure.States;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
@@ -16,21 +17,28 @@ public class MenuState extends State {
 
     @Override
     protected void handleInput() {
-
+        if(Gdx.input.justTouched()){        //TODO should be implemented correctly with the mouse listener
+            gsm.set(new PlayState(gsm));
+        }
     }
 
     @Override
     public void update(float dt) {
-
+        handleInput();
     }
 
     @Override
     public void render(SpriteBatch sb) {
-
+        sb.setProjectionMatrix(cam.combined);
+        sb.begin();
+        sb.draw(background, 0, 0);
+        sb.draw(playBtn, cam.position.x - playBtn.getWidth() / 2, cam.position.y);
+        sb.end();
     }
 
     @Override
     public void dispose() {
-
+        background.dispose();
+        playBtn.dispose();
     }
 }
