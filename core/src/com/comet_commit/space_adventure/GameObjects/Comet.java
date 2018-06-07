a7d70bd2b137f4958c2858ae0a2806d99e33fb7d
Moritz Andres <moritz@moritzandres.net>
2018-06-07 19:10:16 +0200

    add comets to PlayState

--- core/src/com/comet_commit/space_adventure/GameObjects/Comet.java
+++ core/src/com/comet_commit/space_adventure/GameObjects/Comet.java
@@ -1,13 +1,26 @@
 package com.comet_commit.space_adventure.GameObjects;
 
+import com.badlogic.gdx.math.Vector3;
+import com.comet_commit.space_adventure.SpaceAdventure;
+
+import java.util.Random;
+
 public class Comet extends GameObject {
 
     public Comet(float x, float y) {
         super(x, y, "comet.png");
+
+        Random r = new Random();
+
+        super.setPosition(new Vector3(SpaceAdventure.WIDTH + 20, r.nextFloat() *(SpaceAdventure.HEIGHT - 20) + 10,0));
+        super.setVelocity(new Vector3(-2*(r.nextFloat()+4),2*r.nextFloat() - 1,0));
     }
 
     @Override
     public void update(float dt) {
+        super.setPosition(new Vector3(super.getPosition().x + super.getVelocity().x,
+                                      super.getPosition().y + super.getVelocity().y,
+                                      0));
 
     }
 
--- core/src/com/comet_commit/space_adventure/States/PlayState.java
+++ core/src/com/comet_commit/space_adventure/States/PlayState.java
@@ -2,19 +2,37 @@ package com.comet_commit.space_adventure.States;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.comet_commit.space_adventure.GameObjects.Comet;
 import com.comet_commit.space_adventure.GameObjects.Rocket;
 import com.comet_commit.space_adventure.SpaceAdventure;
+
+import java.util.ArrayList;
 
 public class PlayState extends State {
 
     private Rocket rocket;
+    private ArrayList<Comet> comets;
+
     private Texture background;
+    private Texture background2;
+    private int bgPosition, bgPosition2;
+    private float time, last_comet_insertion, comet_interval;
 
     public PlayState(GameStateManager gsm) {
         super(gsm);
 
+        bgPosition = 0;
+        bgPosition2 = SpaceAdventure.WIDTH;
         background = new Texture("sky.png");
-        rocket = new Rocket(SpaceAdventure.WIDTH / 10f, SpaceAdventure.HEIGHT / 2);
+        background2 = new Texture("sky2.png");
+
+        time = 0;
+        last_comet_insertion = 0;
+        comet_interval = 1;
+
+        comets = new ArrayList<Comet>();
+
+        rocket = new Rocket(SpaceAdventure.WIDTH / 20f, SpaceAdventure.HEIGHT / 2);
     }
 
     @Override
@@ -32,6 +50,27 @@ public class PlayState extends State {
         handleInput();
 
         rocket.update(dt);
+
+        for(int i = 0; i < comets.size(); i++){
+            comets.get(i).update(dt);
+
+            if(comets.get(i).getPosition().x < -200) {
+                comets.get(i).dispose();
+                comets.remove(i);
+            }
+        }
+
+        time += dt;
+        if(time >= last_comet_insertion + comet_interval){
+            comets.add(new Comet(150,150));
+            last_comet_insertion = time;
+        }
+
+
+        if(bgPosition <= -1*SpaceAdventure.WIDTH) bgPosition = SpaceAdventure.WIDTH + bgPosition2;
+        if(bgPosition2 <= -1*SpaceAdventure.WIDTH) bgPosition2 = SpaceAdventure.WIDTH + bgPosition;
+        bgPosition -= Math.round(dt*50);
+        bgPosition2 -= Math.round(dt*50);
     }
 
     @Override
@@ -40,15 +79,25 @@ public class PlayState extends State {
         sb.setProjectionMatrix(cam.combined);
         sb.begin();
 
-        sb.draw(background, 0, 0, SpaceAdventure.WIDTH, SpaceAdventure.HEIGHT);
+        sb.draw(background, bgPosition, 0, SpaceAdventure.WIDTH, SpaceAdventure.HEIGHT);
+        sb.draw(background2, bgPosition2, 0, SpaceAdventure.WIDTH, SpaceAdventure.HEIGHT);
+
+        for(Comet comet : comets){
+            sb.draw(comet.getTexture(), comet.getPosition().x, comet.getPosition().y, 120, 120);
+        }
+
         sb.draw(rocket.getTexture(), rocket.getPosition().x, rocket.getPosition().y);
+
 
         sb.end();
     }
 
     @Override
     public void dispose() {
-
         rocket.dispose();
+
+        for(Comet comet : comets)
+            comet.dispose();
+
     }
 }
