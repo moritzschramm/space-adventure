d33550d114b2b5392ab44e765a19425fd0426427
Moritz Schramm <moritz@moritz-schramm.com>
2018-10-10 20:08:24 +0200

    save highscore and show last highscore on screen

--- core/src/com/comet_commit/space_adventure/States/GameOverState.java
+++ core/src/com/comet_commit/space_adventure/States/GameOverState.java
@@ -1,5 +1,7 @@
 package com.comet_commit.space_adventure.States;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Preferences;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -11,6 +13,7 @@ public class GameOverState extends State {
     private Background background;
     private String gameover_str;
     private int latest_score;
+    private int highscore;
 
     public GameOverState(GameStateManager gsm, AssetManager assetManager, int startBgAt, int score, int go_reason) {
         super(gsm, assetManager);
@@ -18,6 +21,16 @@ public class GameOverState extends State {
         background = new Background(startBgAt);
 
         latest_score = score;
+
+        Preferences prefs = Gdx.app.getPreferences("game_state");
+        highscore = prefs.getInteger("highscore", 0);
+
+        if(score > highscore) {
+
+            prefs.putInteger("highscore", score);
+            prefs.flush();
+            highscore = score;
+        }
 
         if(go_reason == 0)      gameover_str = "rocket crashed";
         else if(go_reason == 1) gameover_str = "lost in space";
@@ -28,8 +41,8 @@ public class GameOverState extends State {
     protected void handleInput() {
         if(super.touchDown[0]) {
 
-            if(tp[0].x < SpaceAdventure.WIDTH/2 + 100 && tp[0].x > SpaceAdventure.WIDTH/2 -100
-                    && tp[0].y < SpaceAdventure.HEIGHT/2 + 100 && tp[0].y > SpaceAdventure.HEIGHT/2 - 100) {
+            if(tp[0].x < SpaceAdventure.WIDTH/2 + 200 && tp[0].x > SpaceAdventure.WIDTH/2 -200
+                    && tp[0].y < SpaceAdventure.HEIGHT/2 + 150 && tp[0].y > SpaceAdventure.HEIGHT/2 - 150) {
 
                 gsm.set(new PlayState(gsm, assetManager, background.getRelativePosition()));
             }
@@ -51,13 +64,17 @@ public class GameOverState extends State {
         background.draw(sb);
 
         assetManager.get("normalFont.ttf", BitmapFont.class).draw(sb, "Score: " + latest_score,
-                                    SpaceAdventure.WIDTH / 2 - 160,
+                                    SpaceAdventure.WIDTH / 2 - 140,
                                     SpaceAdventure.HEIGHT / 2 - 80);
 
-        assetManager.get("normalFont.ttf", BitmapFont.class).draw(sb, "play again >", SpaceAdventure.WIDTH / 2 - 200, SpaceAdventure.HEIGHT/2 + 10);
+        assetManager.get("normalFont.ttf", BitmapFont.class).draw(sb, "Highscore: " + highscore,
+                SpaceAdventure.WIDTH / 2 - 200,
+                SpaceAdventure.HEIGHT / 2 - 150);
+
+        assetManager.get("normalFont.ttf", BitmapFont.class).draw(sb, "play again >", SpaceAdventure.WIDTH / 2 - 160, SpaceAdventure.HEIGHT/2 + 10);
             // Instead of drawing the string "Play again", we could draw a reverse-symbol (solves Problem: where to click)
 
-        assetManager.get("bigFont.ttf", BitmapFont.class).draw(sb, gameover_str, SpaceAdventure.WIDTH/ 2  - 280,
+        assetManager.get("bigFont.ttf", BitmapFont.class).draw(sb, gameover_str, SpaceAdventure.WIDTH/ 2  - 300,
                                                    SpaceAdventure.HEIGHT - 150);
 
         sb.end();
